6040df78713b6e03f847776ef8f952c4bac59ce4
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2024-03-09 12:14:51 +0200

    error handling for input and output to resources

--- __main__.py
+++ __main__.py
@@ -1,23 +1,24 @@
+from model.news_data import NewsData
 from scrapers.base_scraper import BaseScraper
 from scrapers.ft_scraper import FinancialTimesScraper
 from scrapers.il_sole_scraper import IlSoleScraper
 from scrapers.scmp_scraper import SouthChinaMorningPostScraper
 import pandas as pd
 from openpyxl import Workbook
-from openpyxl import load_workbook
+from datetime import datetime
 import json
+import os
 
 last_scrape_dates_file = "sources/previous_scrape_dates.json"
 companies_list_file = "sources/companies_list.xlsx"
-report_file = 'reports/report.xlsx'
+report_file = 'reports/report'
 
 scrapers = [IlSoleScraper, FinancialTimesScraper, SouthChinaMorningPostScraper]
 
 
 def main():
     last_scrape_dates = get_previous_dates(last_scrape_dates_file)
-    #companies = load_companies(companies_list_file)
-    companies = ['apple','amazon','tesla']
+    companies = load_companies(companies_list_file)
     news_list = get_articles(companies, scrapers, last_scrape_dates)
     save_results(news_list)
 
@@ -40,9 +41,16 @@ def save_previous_dates(file_path, data):
 
 
 def load_companies(file_path):
-    df = pd.read_excel(file_path)
-    first_column_values = df.iloc[:, 0].tolist()
-    return first_column_values
+    if not os.path.exists(file_path):
+        data = {'Companies list': []}
+        df = pd.DataFrame(data)
+        df.to_excel(file_path, index=False)
+
+    df = pd.read_excel(file_path, engine='openpyxl')
+    if df.empty:
+        raise Exception('No companies to scrape for! Please fill /sources/companies_list.xlsx')
+
+    return df.iloc[:, 0].tolist()
 
 
 def get_articles(queries, article_sources: [BaseScraper], last_scrape_dates):
@@ -64,43 +72,48 @@ def get_articles(queries, article_sources: [BaseScraper], last_scrape_dates):
     return articles_list
 
 
-def save_results(news_list):
+def save_results(news_list: [NewsData]):
     data = []
+    print(f'length of news_list: {len(news_list)}')
     for news in news_list:
+        print(f'number per news: {len(news.articles)}')
         for article in news.articles:
-            news_data = {'Company': news.company, 'Source': news.source, 'Title': article.title, 'Link': article.link, 'Date': article.date}
+            news_data = {'Company': news.company, 'Source': news.source, 'Title': article.title, 'Link': article.link,
+                         'Date': article.date}
             data.append(news_data)
 
-    df = pd.DataFrame(data)
-    try:
-        # Try loading the existing workbook
-        workbook = load_workbook(report_file)
-    except FileNotFoundError:
-        # If the file doesn't exist, create a new workbook
-        workbook = Workbook()
+    if len(data) == 0:
+        print('No new articles were found for these companies since last scrape!\nReport will not be created ...')
+        return
 
-        # Group data by company
+    df = pd.DataFrame(data)
+    workbook = Workbook()
+
+    # Group data by company
     grouped_data = df.groupby('Company')
 
     # Iterate over each group (company) and write data to separate sheets
     for company, group in grouped_data:
         # Check if the sheet already exists
         if company in workbook.sheetnames:
-            sheet = workbook[company]
+            sheet = workbook[company[:31]]
         else:
             # If the sheet doesn't exist, create a new one
             sheet = workbook.create_sheet(title=company)
 
-        # Write data to the sheet
-        if sheet.max_row == 1:  # Check if the header row is already present
-            header_row = ['Source','Title', 'Link', 'Date']
-            sheet.append(header_row)
+        header_row = ['Source', 'Title', 'Link', 'Date']
+        sheet.append(header_row)
 
         for _, row in group.iterrows():
-            sheet.append([row['Source'],row['Title'], row['Link'], row['Date']])
+            sheet.append([row['Source'], row['Title'], row['Link'], row['Date']])
 
-    # Save the workbook
-    workbook.save(report_file)
+    if not os.path.exists('reports'):
+        os.makedirs('reports')
+
+    if 'Sheet' in workbook.sheetnames:
+        workbook.remove(workbook['Sheet'])
+
+    workbook.save(f'{report_file}@{datetime.now().strftime("%Y-%m-%d_%H-%M")}.xlsx')
 
 
 main()
