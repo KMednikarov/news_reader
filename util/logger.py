d0712f918f0923d13c39d751254aa2fb9d23dc12
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2024-03-12 09:49:58 +0200

    logger delete old logs on start

--- util/logger.py
+++ util/logger.py
@@ -1,5 +1,4 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 import os
 log_name = 'logs/execution.log'
 
@@ -8,17 +7,15 @@ if not os.path.exists('logs'):
 
 log_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-file_handler = TimedRotatingFileHandler(log_name, when='D', interval=1, backupCount=1, encoding='utf-8', delay=False)
+file_handler = logging.FileHandler(log_name, mode='w', encoding='utf-8')
 file_handler.setFormatter(log_formatter)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
 
-logging.basicConfig(level=logging.INFO
-                    , format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 log = logging.getLogger('my_logger')
-file_handler.doRollover()
 log.addHandler(file_handler)
 
 #log.addHandler(console_handler)
